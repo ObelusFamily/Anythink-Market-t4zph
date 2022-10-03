bfad149ae1f4a844721e1c730a702e838f7ab8a8
Tan Shun Yuan <tanshunyuan99@gmail.com>
2022-10-03 14:19:07 +0800

    Revert "INC-5312: Fix missing image"

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import { HandleEmptyImageUrl } from "../helpers/handleEmptyImageUrl.js";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -30,8 +29,6 @@ const ItemPreview = (props) => {
     }
   };
 
-  const itemImage = HandleEmptyImageUrl(item.image);
-
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -39,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={itemImage}
+        src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
--- frontend/src/helpers/handleEmptyImageUrl.js
+++
@@ -1,5 +0,0 @@
-export const HandleEmptyImageUrl = (url = "") => {
-  const isEmpty = url.length === 0;
-  const placeholderImagePath = "/placeholder.png";
-  return !isEmpty ? url : placeholderImagePath;
-};

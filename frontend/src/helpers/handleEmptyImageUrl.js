5371c31792678d8178c26b04a6dd974338570c99
Shun Yuan <tanshunyuan99@gmail.com>
2022-10-03 13:04:43 +0800

    chore: provide default param for image handler

--- frontend/src/helpers/handleEmptyImageUrl.js
+++ frontend/src/helpers/handleEmptyImageUrl.js
@@ -1,5 +1,5 @@
-export const HandleEmptyImageUrl = (url) => {
-    const isEmpty = url.length == 0;
+export const HandleEmptyImageUrl = (url='') => {
+    const isEmpty = url.length === 0;
     const placeholderImagePath = '/placeholder.png'
     return !isEmpty ? url : placeholderImagePath
 }
